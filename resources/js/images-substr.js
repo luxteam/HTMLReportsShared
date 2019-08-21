197f2a60d65279e3baa1b75b361a6b145bd507f1
Andrey Alekseevich <magister.demens@gmail.com>
2019-08-21 16:52:03 +0300

    Img diff canvas rendering upd

--- resources/js/images-substr.js
+++ resources/js/images-substr.js
@@ -17,6 +17,29 @@ function renderCanvasData(baselineId, renderId, diffCanvasId, thresholdValue) {
     var imgData2 = ctx.getImageData(0, 0, diffCanvas.width, diffCanvas.height);
 
     var diff = ctx.createImageData(diffCanvas.width, diffCanvas.height);
-    pixelmatch(imgData1.data, imgData2.data, diff.data, diffCanvas.width, diffCanvas.height, {threshold: thresholdValue});
+    pixelmatch(imgData1.data, imgData2.data, diff.data, diffCanvas.width, diffCanvas.height, {threshold: thresholdValue, includeAA: true});
     ctx.putImageData(diff, 0, 0);
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function waitImgLoadAndRenderCanvasData(baselineId, renderId, diffCanvasId, thresholdValue) {
+    var loaded = false;
+    var i = 0;
+    while(i < 1000) {
+        console.log(document.getElementById(baselineId).complete && document.getElementById(renderId).complete);
+        if (document.getElementById(baselineId).complete && document.getElementById(renderId).complete) {
+            console.log("Imgs loaded");
+            renderCanvasData(baselineId, renderId, diffCanvasId, thresholdValue);
+            return;
+        }
+        else {
+            i++;
+            await sleep(1000);
+            console.log("Imgs not loaded");
+        }
+    }
 }
